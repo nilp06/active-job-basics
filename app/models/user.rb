3643ebacef74392ad3544e9edd16732e4d828709
nilpatel <nil.p@simformsolutions.com>
2023-06-12 14:21:51 +0530

    feat: add create employee job

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,15 @@ class User < ApplicationRecord
   has_one :employee, dependent: :destroy
   has_and_belongs_to_many :events
 
-  after_create :send_email
+  after_create :send_email, :create_employee
 
   private
 
   def send_email
     SendWelcomeEmailJob.set(wait: 1.minute).perform_later(self)
+  end
+
+  def create_employee
+    CreateEmployeeJob.set(wait: 2.minute).perform_later(self)
   end
 end
